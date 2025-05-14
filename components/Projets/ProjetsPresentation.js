af19b37d188220071a1dc7124cee876ced85f871
LucasMillart <lucas.millart@gmail.com>
2025-05-14 13:09:19 +0200

    Change Projects Presentation

--- components/Projets/ProjetsPresentation.js
+++ components/Projets/ProjetsPresentation.js
@@ -80,7 +80,7 @@ export default function ProjetsPresentation() {
               <td className="px-2"><span className="badge badge-success">Nouveau</span></td>
               <td className="px-2">Fév 2025</td>
               <td className="px-2">
-                <Link href="https://planifeo.vercel.app" target="_blank" className="btn btn-sm btn-outline">
+                <Link href="projets/planifeo" className="btn btn-sm btn-outline">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 mr-1">
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                     <polyline points="15 3 21 3 21 9"></polyline>
@@ -90,7 +90,7 @@ export default function ProjetsPresentation() {
                 </Link>
               </td>
               <td className="px-2">
-                <Link href="https://github.com/LucasMillart/planifeo" target="_blank" className="btn btn-sm btn-outline">
+                <Link href="/projets/planifeo" className="btn btn-sm btn-outline">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 mr-1">
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
@@ -120,7 +120,7 @@ export default function ProjetsPresentation() {
               <td className="px-2"><span className="badge badge-success">Nouveau</span></td>
               <td className="px-2">Avril 2025</td>
               <td className="px-2">
-                <Link href="https://snake-game.vercel.app" target="_blank" className="btn btn-sm btn-outline">
+                <Link href="https://snake-lm.vercel.app" target="_blank" className="btn btn-sm btn-outline">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 mr-1">
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                     <polyline points="15 3 21 3 21 9"></polyline>
@@ -130,7 +130,7 @@ export default function ProjetsPresentation() {
                 </Link>
               </td>
               <td className="px-2">
-                <Link href="https://github.com/LucasMillart/snake-game" target="_blank" className="btn btn-sm btn-outline">
+                <Link href="https://github.com/LucasMillart/snake" target="_blank" className="btn btn-sm btn-outline">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 mr-1">
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
